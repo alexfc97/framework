612b970dcaee5b032153dff86e4a28bf39d6d15b
Alexander <alexfc@live.dk>
2020-03-31 16:49:57 +0200

    changed Tables.vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import DataTable from 'laravel-vue-datatable';
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 
 Vue.config.productionTip = false;
+Vue.use(DataTable);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app');
