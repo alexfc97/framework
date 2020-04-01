29683d789f5990eed7e10f06fb7190e525aed618
Alexander <alexfc@live.dk>
2020-04-01 12:04:11 +0200

    adding functionality for changing between tables

---
+++ frontend/src/components/MainPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="main">
+        <SelectTable class="select"/>
+        <Tables class="tables" table=""/>
+    </div>
+</template>
+
+<script>
+    import Tables from "./Tables";
+    import SelectTable from "./SelectTable";
+
+    export default {
+        name: "main",
+        components: {
+            SelectTable,
+            Tables
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/SelectTable.vue
@@ -0,0 +1,53 @@
+<template>
+    <section>
+        <h3 align="center">
+            Please choose what table you would like to view!
+        </h3>
+        <div class="col-4 offset-4">
+            <b-form-select v-model="selected" :options="options"></b-form-select>
+            <div align="center" class="mt-3">
+                Selected:
+                <strong>{{ selected }}
+                </strong>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "radioButtons",
+        data() {
+            return {
+                selected:null,
+                options: [
+                    {
+                        value: null,
+                        text:'Please select an option'
+                    },
+                    {
+                        value: 'Measurements',
+                        text: 'Measurements Table'
+                    },
+                    {
+                        value: 'Sensors',
+                        text: 'Sensor Table'
+                    },
+                    {
+                        value: 'Sensor Types',
+                        text: 'Sensor Types'
+                    },
+                    {
+                        value: 'Measurement Types',
+                        text: 'Measurement Types'
+                    }
+                ]
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
