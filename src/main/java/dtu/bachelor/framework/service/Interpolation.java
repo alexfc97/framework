4f786942fcbe81818e106b0ddc870c6c1fc72f00
Alexander <alexfc@live.dk>
2020-04-08 19:16:04 +0200

    added Interpolation class

--- src/main/java/dtu/bachelor/framework/service/Interpolation.java
+++ src/main/java/dtu/bachelor/framework/service/Interpolation.java
@@ -1,4 +1,18 @@
 package dtu.bachelor.framework.service;
 
+import java.util.List;
+
 public class Interpolation {
+
+    void Interpolate(List<Double> list, String interpolatetype) {
+        switch (interpolatetype.toLowerCase()){
+            case "linear":
+                break;
+            case "cubic":
+                break;
+            default:
+                // This will interpolate the geolocations as quadratic
+                break;
+        }
+    }
 }
